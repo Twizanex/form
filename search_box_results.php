684805bdb5759be0d7a6c4f4a0d003c1e1235ea4
olairmarcos <olairmarcos@gmail.com>
2016-08-08 23:43:44 -0300

    Update search_box_results.php
    Fix function call

--- search_box_results.php
+++ search_box_results.php
@@ -18,7 +18,7 @@ require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
 require_once(dirname(__FILE__)."/models/model.php");
     
 // Define context
-set_context('form:content');
+elgg_set_context('form:content');
 
 $form_id = get_input('id',0);
 $offset = get_input('offset',0);
@@ -27,13 +27,13 @@ $tag = get_input('tag','');
 $form = get_entity($form_id);
 
 if ($form && $form->getSubtype() == 'form:form') {
-    set_page_owner(get_loggedin_userid());
+    elgg_set_page_owner_guid(elgg_get_logged_in_user_guid());
     
     $title = sprintf(elgg_echo('form:search_box_results_title'),form_form_t($form,'title'));  
     $entities =  form_search_box_results($form,$tag,$offset,$limit);
     $count =  form_search_box_count($form,$tag);
     $body = form_view_entity_list($entities, $form, $count, $offset, $limit, false, false); 
-    page_draw($title,elgg_view_layout("two_column_left_sidebar", '<br />'.elgg_view("form/search_box",array('form_id'=>$form_id)), elgg_view_title($title) . $body));
+    elgg_view_page($title,elgg_view_layout("two_column_left_sidebar", '<br />'.elgg_view("form/search_box",array('form_id'=>$form_id)), elgg_view_title($title) . $body));
 } else {
     register_error(elgg_echo('form:not_found'));
     forward();
