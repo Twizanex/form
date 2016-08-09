f84d26b84b9e536db897bdc7140b4ab748104266
olairmarcos <olairmarcos@gmail.com>
2016-08-08 22:39:48 -0300

    Fix elgg1.12.x
    Update Function call

--- manage_all_forms.php
+++ manage_all_forms.php
@@ -14,7 +14,7 @@
 require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
     
 // Define context
-set_context('form:admin');
+elgg_set_context('form:admin');
 
 global $CONFIG;
 
@@ -24,17 +24,17 @@ $username = get_input('username');
 if ($username) {
 	$user = get_user_by_username($username);
 } else {
-	$user = get_loggedin_user();
+	$user = elgg_get_logged_in_user_entity();
 }
 
 if ($user) {    
-    set_page_owner($user->getGUID());
+    elgg_set_page_owner_guid($user->getGUID());
 
     $body = elgg_view('form/forms/manage_all_forms',array('username'=>$username));
     
     $title = elgg_echo('form:manage_forms_title');
     
-    page_draw($title,elgg_view_layout("two_column_left_sidebar", '', elgg_view_title($title) . $body));
+    elgg_view_page($title,elgg_view_layout("two_column_left_sidebar", '', elgg_view_title($title) . $body));
 } else {
     register_error(elgg_echo('form:error_no_such_user'));
     forward();
