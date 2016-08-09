095ea16923b09677946a8412a378a318d5d0e9c1
olairmarcos <olairmarcos@gmail.com>
2016-08-08 22:14:15 -0300

    Add files
    Add

---
+++ views/default/form/css.php
@@ -0,0 +1,19 @@
+.river_object_form_data_create {
+	background: url(<?php echo $vars['url']; ?>_graphics/river_icons/river_icon_blog.gif) no-repeat left -1px;
+}
+.river_object_form_data_comment {
+	background: url(<?php echo $vars['url']; ?>_graphics/river_icons/river_icon_comment.gif) no-repeat left -1px;
+}
+
+.form_listing .search_listing {
+	border:2px solid #cccccc;
+	margin:0 0 5px 0;
+}
+
+#groups_info_column_left .tabberlive .tabbertab {
+	background-color: white;
+}
+
+#profile_info_column_middle .tabberlive .tabbertab {
+	background-color: white;
+}
