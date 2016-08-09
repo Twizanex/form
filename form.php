94d9ea3422de39692f804845f3d49c73ee981617
olairmarcos <olairmarcos@gmail.com>
2016-08-08 23:47:38 -0300

    Update form.php
    Fix function call

--- form.php
+++ form.php
@@ -18,7 +18,7 @@ require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
 require_once(dirname(__FILE__)."/models/model.php");
     
 // Define context
-set_context('form:content');
+elgg_set_context('form:content');
 
 $form_id = get_input('id',0);
 $form_data_id = get_input('d',0);
@@ -27,7 +27,7 @@ $preview = get_input('preview',0);
 $form = get_entity($form_id);
 
 if ($form && $form->type == 'object' && get_subtype_from_id($form->subtype) == 'form:form') {
-    set_page_owner($form->owner_guid);
+    elgg_set_page_owner_guid($form->owner_guid);
     if ($form_data_id && ($form_data = form_get_data($form_data_id))) {
         if (get_entity($form_data_id)->canEdit()) {
             $tab_data = form_get_data_for_edit_form($form,$form_data);
@@ -44,7 +44,7 @@ if ($form && $form->type == 'object' && get_subtype_from_id($form->subtype) == '
     $pg_owner_entity = elgg_get_page_owner_entity();
     $username = $pg_owner_entity->username;
  
-    page_draw($title,elgg_view_layout("two_column_left_sidebar", '<br />'.elgg_view("form/search_box",array('form_id'=>$form_id)), elgg_view_title($title) . $body));
+    elgg_view_page($title,elgg_view_layout("two_column_left_sidebar", '<br />'.elgg_view("form/search_box",array('form_id'=>$form_id)), elgg_view_title($title) . $body));
 } else {
     register_error(elgg_echo('form:not_found'));
     forward();
