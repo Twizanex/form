a4c279c1b7c0ed23c278dddfa8e57c223f548e8b
olairmarcos <olairmarcos@gmail.com>
2016-08-08 23:12:43 -0300

    Update display_search_object.php
    Fix function call

--- display_search_object.php
+++ display_search_object.php
@@ -14,7 +14,7 @@
 require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
     
 // Define context
-set_context('form:content');
+elgg_set_context('form:content');
 
 global $CONFIG;
 
@@ -33,6 +33,6 @@ $body = elgg_view('form/display_search_object',array('form_id'=>$form_id, 'form_
 
 $title = elgg_echo('form:search_results_title');
 
-page_draw($title,elgg_view_layout("two_column_left_sidebar", '', elgg_view_title($title) . $body));
+elgg_view_page($title,elgg_view_layout("two_column_left_sidebar", '', elgg_view_title($title) . $body));
 
 ?>
